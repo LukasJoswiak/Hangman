196e938d27a8b99330520d4d08f6cce1819eec82
Lukas <admin@lukasjoswiak.com>
2014-01-10 21:00:18 -0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Hangman
+=================
+
+A simple online Hangman game.
+
+My goal in making this is to learn more and get familiar with vanilla Javascript.
+
+*View the latest live demo at [http://lpjoswiak.com/hangman/](http://lpjoswiak.com/hangman/).*
---
+++ js/index.js
@@ -0,0 +1,48 @@
+function word(input, img) {
+	/*var text = input.value,
+		source = img.src.split('/');
+
+	source = source[source.length - 1];
+
+	if(source === "hideWord.png" && input.type !== "password") {
+		input.type = "password";
+	} else {
+		input.type = "text";
+	}*/
+}
+
+function showHide(input, img) {
+	var text = input.value,
+		source = img.src.split('/');
+
+	source = source[source.length - 1];
+
+	if(source === "hideWord.png") {
+		input.type = "text";
+		img.src = "img/showWord.png";
+		cursorEnd(input);
+	} else {
+		input.type = "password";
+		img.src = "img/hideWord.png";
+		cursorEnd(input);
+	}
+}
+
+var input = document.getElementById('word'),
+	img	  = document.getElementById('hide');
+
+input.addEventListener('keyup', function() { word(input, img) }, false);
+img.addEventListener('mouseup', function() { showHide(input, img) }, false);
+
+// http://stackoverflow.com/a/4331028/986991
+function cursorEnd(input) {
+	console.log(input);
+	if(input.setSelectionRange) {
+		var len = input.value.length * 2;
+		input.setSelectionRange(len, len);
+	} else {
+		input.value = input.value;
+	}
+
+	input.scrollLeft = 999999;
+}
